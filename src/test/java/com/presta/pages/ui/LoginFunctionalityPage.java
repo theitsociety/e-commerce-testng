bd083dfbf16e7a956a67af903e503179b90df6d8
Mehmet Gul <mehmet@clarusway.com>
2023-06-15 22:20:34 -0400

    Login func development

--- src/test/java/com/presta/pages/ui/LoginFunctionalityPage.java
+++ src/test/java/com/presta/pages/ui/LoginFunctionalityPage.java
@@ -5,10 +5,19 @@ import com.presta.utility.ConfigReader;
 import com.presta.utility.Utility;
 
 public class LoginFunctionalityPage extends Utility {
-    public String url= ConfigReader.getProperty("url");
+    /**
+     * Given User is on the homepage
+     * When user clicks on Sing In link
+     * Then User will forward to Login Page
+     * And User enters the email and password
+     *  |testuser@gmail.com|test1234|
+     * And user clicks sing in button
+     * Then user should be able to see the "My account - My Store" title
+     * Then user should be able to verify the "Welcome to your account. Here you can manage all of your personal information and orders."
+     */
     LoginFunctionalityElement loginFunctionalityElement =new LoginFunctionalityElement();
     public void loginFunctionalityTest(){
-       // clickElement(loginFunctionalityElement.click);
+
 
     }
 
