056a736e6a2bc43509645df39620e080244f842b
Mehmet Gul <mehmet@clarusway.com>
2023-06-16 00:09:25 -0400

    Login feature finished

--- src/test/java/com/presta/pages/ui/LoginFunctionalityPage.java
+++ src/test/java/com/presta/pages/ui/LoginFunctionalityPage.java
@@ -1,24 +1,35 @@
 package com.presta.pages.ui;
 
+import com.github.javafaker.Faker;
 import com.presta.elements.ui.LoginFunctionalityElement;
 import com.presta.utility.ConfigReader;
 import com.presta.utility.Utility;
+import org.testng.Assert;
+import org.testng.asserts.SoftAssert;
 
 public class LoginFunctionalityPage extends Utility {
-    /**
-     * Given User is on the homepage
-     * When user clicks on Sing In link
-     * Then User will forward to Login Page
-     * And User enters the email and password
-     *  |testuser@gmail.com|test1234|
-     * And user clicks sing in button
-     * Then user should be able to see the "My account - My Store" title
-     * Then user should be able to verify the "Welcome to your account. Here you can manage all of your personal information and orders."
-     */
+
+    SoftAssert sf = new SoftAssert();
     LoginFunctionalityElement loginFunctionalityElement =new LoginFunctionalityElement();
+
     public void loginFunctionalityTest(){
-
-
+        String testEmail=ConfigReader.getProperty("testEmail");
+        String testPassword=ConfigReader.getProperty("testPassword");
+        clickElement(loginFunctionalityElement.signInLink);
+        waits(2);
+        sendKeyToElement(loginFunctionalityElement.singInEmail,testEmail);
+        waits(2);
+        sendKeyToElement(loginFunctionalityElement.singInPassword,testPassword);
+        waits(2);
+        clickElement(loginFunctionalityElement.singInButton);
+        waits(2);
+        //Actual and Expected result
+        String expectedAccountDescription="Welcome to your account. Here you can manage all of your personal information and orders.";
+        String actualAccountDescription=getTextElement(loginFunctionalityElement.myAccountDescription);
+        Assert.assertEquals(actualAccountDescription,expectedAccountDescription,"Descriptions are not matching");
+       //Soft Assertion
+        sf.assertEquals(actualAccountDescription,expectedAccountDescription,"Descriptions are not matching");
+        sf.assertAll();
     }
 
 }
